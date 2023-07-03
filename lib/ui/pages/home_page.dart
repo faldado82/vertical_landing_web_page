6ed01e40fa0b62e80b52eb6c7ada28c6004c7bfa
faldado82 <faldado82@gmail.com>
2023-07-03 13:57:56 -0300

    google fonts package, home view

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_web_page/ui/views/home_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      body: Stack(
+        children: [
+          _HomeBody(),
+          Positioned(
+              right: 20,
+              top: 20,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.black,
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: const [
+        HomeView(),
+      ],
     );
   }
 }
